2254866b5f71636b31c36e5e123fbc4e36b2dd63
limabeans <toomanytypos@gmail.com>
2015-01-17 21:36:11 -0800

    so I think I got time logging working, and without redundant websites!

--- background.js
+++ background.js
@@ -12,19 +12,35 @@ chrome.browserAction.onClicked.addListener(function() {
 
 
 //Listener: onUpdated 
-//When the user changes url, tends to be called multiple times.
+//When the user changes url. Tends to be called multiple times.
 chrome.tabs.onUpdated.addListener( function(tabId, changeInfo, tab) {
     //Don't want to be concerned with websites that aren't loaded.
-    if(changeInfo.status === 'complete' && !ignoreTheseWebsites(tab)) {
+    if(changeInfo.status === 'complete') {
 	var entry = new Entry(new Date(), 0,
 			      'top domain here', 'tag', 
 			      tab.url, tab.title );
 	if(currEntry==null) {
-	    currEntry=entry;
+	    if(!ignoreTheseWebsites(tab)) {
+		currEntry=entry;
+		console.log('denullified- ' + currEntry.title);
+	    } 
+	    //Otherwise, currEntry remains as null.
+	} else if(entry.url !== currEntry.url) {
+	    //This means that they changed the url.
+	    currEntry.timeRange.ended = new Date();
+	    var startMillis = currEntry.timeRange.started.getTime();
+	    var endMillis = currEntry.timeRange.ended.getTime();
+	    currEntry.totalTime = endMillis - startMillis;
+	    updateList(currEntry);
+	    console.log('[updated]');
+	    //But that url might not be worth making 
+	    //a new entry for.
+	    if(!ignoreTheseWebsites(tab)) {
+		currEntry = entry;
+	    } else {
+		currEntry = null;
+	    }
 	}
-	
-	updateList(entry);
-	console.log('[updated]');
     }
 });
 
@@ -32,26 +48,38 @@ chrome.tabs.onUpdated.addListener( function(tabId, changeInfo, tab) {
 //When the user switches tabs.
 chrome.tabs.onActivated.addListener(function(activeInfo) {
     chrome.tabs.get(activeInfo.tabId, function(tab) {
-	if(ignoreTheseWebsites(tab)) {
-	    return;
-	}
 	var entry = new Entry(new Date(), 0,
 			      'top domain here', 'tag', 
 			      tab.url, tab.title );
-
 	if(currEntry==null) {
-	    currEntry=entry;
+	    if(!ignoreTheseWebsites(tab)) {
+		currEntry=entry;
+		console.log('denullified- ' + currEntry.title);
+	    } 
+	    //Otherwise, currEntry remains as null.
+	} else if(entry.url !== currEntry.url) {
+	    //This means that they switched tabs.
+	    currEntry.timeRange.ended = new Date();
+	    var startMillis = currEntry.timeRange.started.getTime();
+	    var endMillis = currEntry.timeRange.ended.getTime();
+	    currEntry.totalTime = endMillis - startMillis;
+	    updateList(currEntry);
+	    console.log('[activated]');
+	    //But that url might not be worth making 
+	    //a new entry for.
+	    if(!ignoreTheseWebsites(tab)) {
+		currEntry = entry;
+	    } else {
+		currEntry = null;
+	    }
 	}
-
-	updateList(entry);
-	console.log('[activated]');
     });
 });
 
 
 //Function responsible for updating the list in timelog.html.
 updateList = function(entry) {
-    var txt = entry.dateOfCreation + ' - ' +
+    var txt = entry.timeRange.started + ' - ' +
 	entry.totalTime + ' - ' + entry.domain + 
 	' - ' + entry.tag + ' - ' + entry.url + 
 	' - ' + entry.title;
@@ -70,18 +98,19 @@ updateList = function(entry) {
 ignoreTheseWebsites = function(tab) {
     if(tab.title === 'Time Log' || tab.title === 'Extensions' ||
        tab.url === 'chrome://newtab/' || tab.title === 'New Tab' || 
-      tab.title === 'Loading...') {
+       tab.title === 'Loading...') {
 	console.log('A website was ignored from logging.');
 	return true;
     }
     return false;
 };
 
-function Entry(dateOfCreate, totTime, domai, ta, ur, tit) {
-    this.dateOfCreation = dateOfCreate;
+function Entry(create, totTime, d, t, u, ti) {
+    this.timeRange = {started: create,
+		      ended: null};
     this.totalTime = totTime;
-    this.domain = domai;
-    this.tag = ta;
-    this.url = ur;
-    this.title = tit;
+    this.domain = d;
+    this.tag = t;
+    this.url = u;
+    this.title = ti;
 }

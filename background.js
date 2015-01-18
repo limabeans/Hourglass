051e6730a34d20ff0085309df0d8b6628fba9544
limabeans <toomanytypos@gmail.com>
2015-01-17 20:54:53 -0800

    invented the Entry object

--- background.js
+++ background.js
@@ -1,40 +1,60 @@
-//Updated vs Activated Listeners
-//Updated: when you change the url, tends to be called more than once
-//Activated: when one switches to tab
+//Holds the current entry.
+var currEntry = null;
 
 //Listener: listening for when the user clicks on the icon
+//and creates new tab
 chrome.browserAction.onClicked.addListener(function() {
-    timelogNewTab();
-});
-
-//function that is responsible for creating the new tab
-timelogNewTab = function() {
     chrome.tabs.create({
 	url: "./timelog.html",
 	active: true
     });
-};
-
-//Listener: onUpdated listener
-chrome.tabs.onUpdated.addListener( function(tabId, changeInfo, tab) {
-    updateList(tab.title);
-    console.log('[updated] ' + tab.title + ' ' + Date.now());
 });
 
-//Listener: onActivated listener
+
+//Listener: onUpdated 
+//When the user changes url, tends to be called multiple times.
+chrome.tabs.onUpdated.addListener( function(tabId, changeInfo, tab) {
+    //Don't want to be concerned with websites that aren't loaded.
+    if(changeInfo.status === 'complete' && !ignoreTheseWebsites(tab)) {
+	var entry = new Entry(new Date(), 0,
+			      'top domain here', 'tag', 
+			      tab.url, tab.title );
+	if(currEntry==null) {
+	    currEntry=entry;
+	}
+	
+	updateList(entry);
+	console.log('[updated]');
+    }
+});
+
+//Listener: onActivated 
+//When the user switches tabs.
 chrome.tabs.onActivated.addListener(function(activeInfo) {
     chrome.tabs.get(activeInfo.tabId, function(tab) {
-	updateList(tab.title);
-	console.log('[activated] ' + tab.title + ' ' + Date.now());
+	if(ignoreTheseWebsites(tab)) {
+	    return;
+	}
+	var entry = new Entry(new Date(), 0,
+			      'top domain here', 'tag', 
+			      tab.url, tab.title );
+
+	if(currEntry==null) {
+	    currEntry=entry;
+	}
+
+	updateList(entry);
+	console.log('[activated]');
     });
 });
 
 
-//function responsible for updating the list in timelog.html
-updateList = function(title) {
-    var time = new Date();
-    var txt = time.getHours() + ':' + time.getMinutes() +
-	' - ' + title;
+//Function responsible for updating the list in timelog.html.
+updateList = function(entry) {
+    var txt = entry.dateOfCreation + ' - ' +
+	entry.totalTime + ' - ' + entry.domain + 
+	' - ' + entry.tag + ' - ' + entry.url + 
+	' - ' + entry.title;
     //Not sure yet why I couldn't have declared this globally.
     //Was getting a port disconnected error.
     var logPort = chrome.runtime.connect({name: 'timelog'});    
@@ -44,5 +64,24 @@ updateList = function(title) {
 	elemType: 'li',
 	text: txt
     });
+    console.log('CURR ENTRY ' + currEntry.title);
 };
 
+ignoreTheseWebsites = function(tab) {
+    if(tab.title === 'Time Log' || tab.title === 'Extensions' ||
+       tab.url === 'chrome://newtab/' || tab.title === 'New Tab' || 
+      tab.title === 'Loading...') {
+	console.log('A website was ignored from logging.');
+	return true;
+    }
+    return false;
+};
+
+function Entry(dateOfCreate, totTime, domai, ta, ur, tit) {
+    this.dateOfCreation = dateOfCreate;
+    this.totalTime = totTime;
+    this.domain = domai;
+    this.tag = ta;
+    this.url = ur;
+    this.title = tit;
+}
